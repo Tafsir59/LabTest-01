9847c015afe4e04f897f7f4746975f5ad23cc1fb
Tafsir59 <it24059@mbstu.ac.bd>
2026-06-10 22:35:07 +0600

    Create UniversityMember.java

---
+++ University_management /UniversityMember.java
@@ -0,0 +1,16 @@
+abstract class UniversityMember {
+    protected int memberId;
+    protected String name;
+
+    public UniversityMember(int memberId, String name) {
+        this.memberId = memberId;
+        this.name = name;
+    }
+
+    public void displayBasicInfo() {
+        System.out.println("ID: " + memberId + ", Name: " + name);
+    }
+
+    // Abstract method
+    public abstract void performDuty();
+}
